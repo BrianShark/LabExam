33e9215f9819175a8d143edee6b932db76833a5f
Brian <briansharkey07@gmail.com>
2021-03-10 10:50:22 +0000

    Final Commit, Ran out of time. managed to write a few tests and gererate a report.

--- src/main/java/ie/gmit/Employee.java
+++ src/main/java/ie/gmit/Employee.java
@@ -26,7 +26,7 @@ public class Employee {
         }
         else
         {
-            throw new IllegalArgumentException("Name provided is not valid.");
+            throw new IllegalArgumentException("Name provided is not valid");
         }
 
 
